47536ebcba5848b456dd16cddbca67daf6eead94
Ayo <bimboladxtreme@gmail.com>
2021-07-30 02:41:23 +0100

    Installed Firebase

---
+++ src/components/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyD13YqLPrR5UdK_80fanyx3ZUcYNBK1cBE",
+	authDomain: "admin-dashboard-86722.firebaseapp.com",
+	projectId: "admin-dashboard-86722",
+	storageBucket: "admin-dashboard-86722.appspot.com",
+	messagingSenderId: "5525507175",
+	appId: "1:5525507175:web:4aa4703ab7f10d919558c2",
+	measurementId: "G-ZP2NDT0DRE",
+};
